1e57e117655d7144fe38be674b85bbcb77297146
lloydbenson <lloyd.benson@gmail.com>
2015-01-27 07:11:50 -0600

    initial commit of hatchery

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hatchery
-test functions for lab
+hatchery
+======
+
+source control for ficion
+
+[![Build Status](https://secure.travis-ci.org/fishin/hatchery.svg)](http://travis-ci.org/fishin/hatchery)
+[![NPM version](https://badge.fury.io/js/hatchery.svg)](http://badge.fury.io/js/hatchery)
+[![devDependency Status](https://david-dm.org/fishin/hatchery/dev-status.svg)](https://david-dm.org/fishin/hatchery#info=devDependencies)
+
---
+++ test/hatchery.js
@@ -0,0 +1,39 @@
+var Code = require('code');
+var Lab = require('lab');
+
+var Hatchery = require('../lib');
+
+var internals = {
+    defaults: {
+        dirPath: __dirname
+    }
+};
+
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var describe = lab.describe;
+var it = lab.it;
+
+describe('hatchery', function () {
+
+    it('getTestResult none', function (done) {
+
+        var hatchery = new Hatchery(internals.defaults);
+        var result = hatchery.getTestResult('example', 'lab1.json');
+        expect(result).to.not.exist();
+        done();
+    });
+
+    it('getTestResult', function (done) {
+
+        var hatchery = new Hatchery(internals.defaults);
+        var result = hatchery.getTestResult('example', 'lab.json');
+        expect(result.totalTests).to.exist();
+        expect(result.tests).to.exist();
+        expect(result.coveragePercent).to.exist();
+        expect(result.coverage).to.exist();
+        expect(result.totalDuration).to.exist();
+        expect(result.totalLeaks).to.exist();
+        done();
+    });
+});
