16e37b482af0be06555ed3f717baca475477ffd8
lloydbenson <lloyd.benson@gmail.com>
2015-09-05 12:38:21 -0500

    start support for tap files

--- test/hatchery.js
+++ test/hatchery.js
@@ -20,29 +20,47 @@ describe('hatchery', function () {
     it('getTestResult none', function (done) {
 
         var hatchery = new Hatchery(internals.defaults);
-        var result = hatchery.getTestResult('lab1.json');
+        var result = hatchery.getTestResult('test.none');
         expect(result).to.not.exist();
         done();
     });
 
-    it('getTestResult broke', function (done) {
+    it('getTestResult lab broke', function (done) {
 
         var hatchery = new Hatchery(internals.defaults);
-        var result = hatchery.getTestResult('broke.json');
+        var result = hatchery.getTestResult('broke.lab');
         expect(result).to.not.exist();
         done();
     });
 
-    it('getTestResult', function (done) {
+    it('getTestResult lab missing tests', function (done) {
 
         var hatchery = new Hatchery(internals.defaults);
-        var result = hatchery.getTestResult('lab.json');
+        var result = hatchery.getTestResult('missingTests.lab');
+        expect(result).to.not.exist();
+        done();
+    });
+
+    it('getTestResult lab', function (done) {
+
+        var hatchery = new Hatchery(internals.defaults);
+        var result = hatchery.getTestResult('test.lab');
         expect(result.totalTests).to.exist();
         expect(result.tests).to.exist();
         expect(result.coveragePercent).to.exist();
         expect(result.coverage).to.exist();
         expect(result.totalDuration).to.exist();
         expect(result.totalLeaks).to.exist();
+        done();
+    });
+
+    it('getTestResult tap', function (done) {
+
+        var hatchery = new Hatchery(internals.defaults);
+        var result = hatchery.getTestResult('test.tap');
+        expect(result.totalTests).to.exist();
+        expect(result.tests).to.exist();
+        expect(result.totalDuration).to.exist();
         done();
     });
 });
